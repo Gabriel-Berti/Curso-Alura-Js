b850b334a30060126b55a6023dc6c025ac0dc761
Gabriel Ramalho Berti <gabriel.berti@linainfratech.com.br>
2021-12-21 13:31:41 -0300

    Aprendendo sobre erros e alguns conceitos do JS

---
+++ conceitos.js
@@ -0,0 +1,11 @@
+// "O JavaScript é uma linguagem de tipagem dinâmica (untyped), ou seja, o tipo de dado de uma variável pode ser definido e alterado sem a necessidade de dizermos qual o tipo do dado que será atribuido a aquela variável"
+
+let minhaVar = 567;
+minhaVar = "texto"
+minhaVar = true
+
+// "Além de ser untyped, o JS também é multiparadigma, ou seja, podemos resolver o mesmo problema de várias formas diferentes, podendo ser funcional, orientado a objetos ou lógico por exemplo. O que torna essa linguagem mais atraente para os iniciantes em programação."
+
+// "O JS é uma linguagem interpretada, ou seja, ela não precisa passar por um compilador, que transforma o código em linguagem de máquina, que apesar de rodar mais rapidamente, torna o código quase ilegivel para nós."
+
+// "O node.js é um interpretador, assim como o browser tem um interpretador de JS, a diferença é que o node permite que o nosso código JS, seja rodado em outros ambientes, principalmente no back-end do lado do servidor."
---
+++ erros.js
@@ -0,0 +1,4 @@
+/* const numero; | SyntaxError: Missing initializer in const declaration.
+console.log(numero | SyntaxError: missing ) after argument list.
+console.log(minhaVar) | ReferenceError: minhaVar is not defined.
+"O Stacktrace, é parte que nos mostra quais comandos e bibliotecas estão sendo utilizadas para rodar o nosso código, ele aparece em baixo dos erros no terminal e pode nos ajudar a descobrir o que está causando o problema." */
